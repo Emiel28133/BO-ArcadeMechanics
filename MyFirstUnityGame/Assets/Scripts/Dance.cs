f295efefd0421e66cec78b54197903315f606e62
Emiel Plokker <28133@ma-web.nl>
2024-03-01 16:54:00 +0100

    commit 2

---
+++ MyFirstUnityGame/Assets/Scripts/Dance.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dance : MonoBehaviour
+{
+    Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+ 
+
+            //Send the message to the Animator to activate the trigger parameter named "Jump"
+            m_Animator.SetTrigger("Dance");
+        }
+    }
+}
