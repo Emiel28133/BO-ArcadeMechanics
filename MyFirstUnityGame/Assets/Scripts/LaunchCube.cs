a79622b04f78f06ad7a4dcc9351076368b97180c
Your Name <leenlaptop>
2024-03-14 12:44:08 +0100

    jump feature gemaakt

--- MyFirstUnityGame/Assets/Scripts/LaunchCube.cs
+++ MyFirstUnityGame/Assets/Scripts/LaunchCube.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class Example : MonoBehaviour
@@ -7,6 +8,8 @@ public class Example : MonoBehaviour
     Rigidbody m_Rigidbody;
     public float m_Thrust = 20f;
     public float gravityScale = 5;
+    private bool onFloor = false;
+    
 
     void Start()
     {
@@ -14,13 +17,40 @@ public class Example : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            onFloor = true;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space) && onFloor == true) ;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            onFloor = false;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space) && onFloor == true) ;
+    }
+
+
+    void Update()
     {
         if (Input.GetButton("Jump"))
         {
             //Apply a force to this Rigidbody in direction of this GameObjects up axis
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
-            m_Rigidbody.AddForce(Physics.gravity * (gravityScale - 1) * m_Rigidbody.mass);
+           // m_Rigidbody.AddForce(transform.up * m_Thrust);
+           // m_Rigidbody.AddForce(Physics.gravity * (gravityScale - 1) * m_Rigidbody.mass);
+            if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
+            {
+                m_Rigidbody.AddForce(transform.up * m_Thrust);
+                m_Rigidbody.AddForce(Physics.gravity * (gravityScale - 1) * m_Rigidbody.mass);
+            }
         }
     }
 }
+
